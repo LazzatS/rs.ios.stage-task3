af4b67c9b037b9ec67c26aae8e3daed062fd26e6
Lazzatios <lseiilova@nu.edu.kz>
2021-05-22 17:36:43 +0600

    feat: convert number to month name

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,7 +5,18 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    
+    NSDateFormatter *monthFormatter = [[NSDateFormatter alloc] init];   // use date formatter
+    [monthFormatter setDateFormat:@"MMM"];                              // use the formate of full month name
+    
+    if (monthNumber <= 0 || monthNumber >= 13) {                        // month cannot be negative or 0th or higher than 12
+        return nil;                                                     // return nill in the case wrong month number is provided and exit
+    }
+    
+    // use month symbols to decode the month number in the order - months start from 0 index i.e. January is NOT 1 but 0 index month
+    NSString *monthString = [[monthFormatter monthSymbols] objectAtIndex:(monthNumber - 1)];
+    
+    return monthString;                                                 // return month name
 }
 
 #pragma mark - Second
